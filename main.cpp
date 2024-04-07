243c3a793cbb9826c0acabd73cb52de5a84582e1
jasmienjas <118551761+jasmienjas@users.noreply.github.com>
2024-04-07 20:54:04 +0300

    Final main.cpp

--- main.cpp
+++ main.cpp
@@ -32,87 +32,7 @@ int heuristic(const Cell& a, const Cell& b) {
     return abs(a.first - b.first) + abs(a.second - b.second);
 }
 
-// Helper function to get unvisited neighbors for maze generation
-vector<Cell> getUnvisitedNeighbors(const vector<vector<bool>>& maze, const Cell& cell) {
-    vector<Cell> neighbors;
-    int x = cell.first, y = cell.second;
-    int rows = maze.size(), cols = maze[0].size();
-
-    // Check left neighbor
-    if (x > 0 && !maze[y][x - 1])
-        neighbors.push_back(make_pair(x - 1, y));
-
-    // Check right neighbor
-    if (x < cols - 1 && !maze[y][x + 1])
-        neighbors.push_back(make_pair(x + 1, y));
-
-    // Check top neighbor
-    if (y > 0 && !maze[y - 1][x])
-        neighbors.push_back(make_pair(x, y - 1));
-
-    // Check bottom neighbor
-    if (y < rows - 1 && !maze[y + 1][x])
-        neighbors.push_back(make_pair(x, y + 1));
-
-    return neighbors;
-}
-
-// Maze generation using Prim's algorithm
-vector<vector<bool>> generateMaze(int width, int height) {
-    vector<vector<bool>> maze(height, vector<bool>(width, false));
-    Cell start_cell = make_pair(1, 1); // Start from (1, 1) to create borders
-    maze[1][1] = true;
-
-    vector<Cell> frontier = {start_cell};
-    while (!frontier.empty()) {
-        int random_index = rand() % frontier.size();
-        Cell current_cell = frontier[random_index];
-        frontier.erase(frontier.begin() + random_index);
-
-        vector<Cell> neighbors = getUnvisitedNeighbors(maze, current_cell);
-        if (!neighbors.empty()) {
-            int random_neighbor_index = rand() % neighbors.size();
-            Cell neighbor = neighbors[random_neighbor_index];
-            int x1 = current_cell.first, y1 = current_cell.second;
-            int x2 = neighbor.first, y2 = neighbor.second;
-
-            // Mark the cells as visited
-            maze[y1][x1] = true;
-            maze[y2][x2] = true;
-
-            // Add the remaining unvisited neighbors to the frontier
-            vector<Cell> new_neighbors = getUnvisitedNeighbors(maze, neighbor);
-            frontier.insert(frontier.end(), new_neighbors.begin(), new_neighbors.end());
-        }
-    }
-
-    // Create borders
-    for (int x = 0; x < width; x++) {
-        maze[0][x] = true; // Top border
-        maze[height - 1][x] = true; // Bottom border
-    }
-    for (int y = 0; y < height; y++) {
-        maze[y][0] = true; // Left border
-        maze[y][width - 1] = true; // Right border
-    }
-
-    return maze;
-}
-
-int main() {
-    srand(time(nullptr));  // Seed the random number generator
-
-    // Generate maze
-    int width, height;
-    cout << "Enter the width of the maze: ";
-    cin >> width;
-    cout << "Enter the height of the maze: ";
-    cin >> height;
-
-    vector<vector<bool>> maze = generateMaze(width, height);
-
-    // TODO: Implement A* algorithm to find the shortest path
-    / Helper function to get neighbors for A* algorithm
+// Get unvisited neighbors of a cell
 vector<Cell> getNeighbors(const vector<vector<bool>>& maze, const Cell& cell) {
     vector<Cell> neighbors;
     int x = cell.first, y = cell.second;
@@ -182,10 +102,96 @@ vector<Cell> aStar(const vector<vector<bool>>& maze, const Cell& start, const Ce
     return vector<Cell>();
 }
 
+// Get unvisited neighbors for maze generation
+vector<Cell> getUnvisitedNeighbors(const vector<vector<bool>>& maze, const Cell& cell) {
+    vector<Cell> neighbors;
+    int x = cell.first, y = cell.second;
+    int rows = maze.size(), cols = maze[0].size();
+
+    // Check left neighbor
+    if (x > 0 && !maze[y][x - 1])
+        neighbors.push_back(make_pair(x - 1, y));
+
+    // Check right neighbor
+    if (x < cols - 1 && !maze[y][x + 1])
+        neighbors.push_back(make_pair(x + 1, y));
+
+    // Check top neighbor
+    if (y > 0 && !maze[y - 1][x])
+        neighbors.push_back(make_pair(x, y - 1));
+
+    // Check bottom neighbor
+    if (y < rows - 1 && !maze[y + 1][x])
+        neighbors.push_back(make_pair(x, y + 1));
+
+    return neighbors;
+}
+
+// Maze generation using Prim's algorithm
+vector<vector<bool>> generateMaze(int width, int height) {
+    vector<vector<bool>> maze(height, vector<bool>(width, false));
+    Cell start_cell = make_pair(1, 1); // Start from (1, 1) to create borders
+    maze[1][1] = true;
+
+    vector<Cell> frontier = {start_cell};
+    while (!frontier.empty()) {
+        int random_index = rand() % frontier.size();
+        Cell current_cell = frontier[random_index];
+        frontier.erase(frontier.begin() + random_index);
+
+        vector<Cell> neighbors = getUnvisitedNeighbors(maze, current_cell);
+        if (!neighbors.empty()) {
+            int random_neighbor_index = rand() % neighbors.size();
+            Cell neighbor = neighbors[random_neighbor_index];
+            int x1 = current_cell.first, y1 = current_cell.second;
+            int x2 = neighbor.first, y2 = neighbor.second;
+
+            // Mark the cells as visited
+            maze[y1][x1] = true;
+            maze[y2][x2] = true;
+
+            // Add the remaining unvisited neighbors to the frontier
+            vector<Cell> new_neighbors = getUnvisitedNeighbors(maze, neighbor);
+            frontier.insert(frontier.end(), new_neighbors.begin(), new_neighbors.end());
+        }
+    }
+
+    // Create borders
+    for (int x = 0; x < width; x++) {
+        maze[0][x] = true; // Top border
+        maze[height - 1][x] = true; // Bottom border
+    }
+    for (int y = 0; y < height; y++) {
+        maze[y][0] = true; // Left border
+        maze[y][width - 1] = true; // Right border
+    }
+
+    return maze;
+}
+
+int main() {
+    srand(time(nullptr));  // Seed the random number generator
+
+    int width, height;
+    cout << "Enter the width of the maze: ";
+    cin >> width;
+    cout << "Enter the height of the maze: ";
+    cin >> height;
+
+    vector<vector<bool>> maze = generateMaze(width, height);
+
+    Cell start = make_pair(1, 1);
+    Cell end = make_pair(width - 2, height - 2);
+
+    vector<Cell> path = aStar(maze, start, end);
+
     // Print the maze and the path
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            if (maze[y][x])
+            Cell cell = make_pair(x, y);
+            if (find(path.begin(), path.end(), cell) != path.end())
+                cout << "*";
+            else if (maze[y][x])
                 cout << "#";
             else
                 cout << " ";
